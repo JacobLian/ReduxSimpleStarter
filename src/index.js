29f15db23ed8b9a1b919b0c7be2480f02f6b6dca
Jacob Aintablian <jacob.aintablian@gmail.com>
2018-08-21 17:19:31 -0600

    d

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyB7f2nJzlzlCdZzVjMCVk116y8AJj_zlEo';
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
-    this.setState({ videos });
-});
+
 
 class App extends Component{
     constructor(props){
         super(props);
 
         this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
+            this.setState({ videos });
+        });
     }
     render() {
     return (
         <div>
             <SearchBar />
+            <VideoList videos={this.state.videos} />
         </div>
     );
 }
